1d01d2cecdf2b490b7dca9ba9e9f56349f4235dc
Guy Avraham <guy1812@gmail.com>
2020-04-11 20:29:53 +0300

    Updates

---
+++ precache-manifest.929c744ca2d1cc85856e71dd28dabe80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fe30b62ccaef589c67a6a4afcaae59a",
+    "url": "/deep-link-redirect/index.html"
+  },
+  {
+    "revision": "7f0a51b8efea863b04c7",
+    "url": "/deep-link-redirect/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f9064ad8f1d627613593",
+    "url": "/deep-link-redirect/static/js/2.5fb3bb67.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deep-link-redirect/static/js/2.5fb3bb67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f0a51b8efea863b04c7",
+    "url": "/deep-link-redirect/static/js/main.2e119f8c.chunk.js"
+  },
+  {
+    "revision": "4fa91a7b954950bdd070",
+    "url": "/deep-link-redirect/static/js/runtime-main.26e44fc8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deep-link-redirect/static/media/logo.5d5d9eef.svg"
+  }
+]);

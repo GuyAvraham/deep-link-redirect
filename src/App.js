27b091e6cb96c746305cfe1ea3b0848303ece3ea
Guy Avraham <guy1812@gmail.com>
2020-04-11 20:16:23 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -3,24 +3,38 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const isIos = !!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);
+  if (isIos) {
+    window.location.href = "kbp://";
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <p>
+            Moving to KB-PURE Iphone App
+          </p>
+          <a className="App-link" href="kbp://">
+            לחצי כאן כדיי לחזור לאפליקציה
+          </a>
+        </header>
+      </div>
+    );
+  } else {
+    window.location.href = "kbp://";
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <p>
+            Go To KB-PURE Android App
+          </p>
+          <a className="App-link" href="kbp://">
+            לחצי כאן כדיי לחזור לאפליקציה
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;

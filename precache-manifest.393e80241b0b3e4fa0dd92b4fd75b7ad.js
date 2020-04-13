509b014e43386f2e2be82616843bb3df0b788da0
Guy Avraham <guy1812@gmail.com>
2020-04-13 18:08:14 +0300

    Updates

---
+++ precache-manifest.393e80241b0b3e4fa0dd92b4fd75b7ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30249e3dcc155649914bd2a199797af7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03102a80e1a72339ff75",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4971bc3efb0328fb3a1f",
+    "url": "/static/js/2.fd962e3f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fd962e3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03102a80e1a72339ff75",
+    "url": "/static/js/main.3f998acf.chunk.js"
+  },
+  {
+    "revision": "fe586cb769234b0c52ba",
+    "url": "/static/js/runtime-main.c85b2129.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);

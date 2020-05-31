4ca3f26263985622987fd7fe42c2f052715f834e
Guy Avraham <guy1812@gmail.com>
2020-05-31 16:25:39 +0300

    add README.md

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 - Allows you to have https url that creates deep link implementation by IOS or Android
 - For a use case where you know they already have on their phones.
 - Not for when you want navigation to the App/Play store.
-
+- Was useful to me when I wanted to point my facebook bot users back to the app they came from
 https://docs.expo.io/workflow/linking/#linking-to-your-app
--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 const appScheme = "kbp://";
+const appName = 'KB-PURE';
+
 function App() {
   const isIos = !!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);
     window.location.href = appScheme;
-    return (
+  return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo"/>
           <p>
-            Moving to KB-PURE {isIos? 'Iphone' : 'Android'} App
+            Moving to {appName} {isIos ? 'Iphone' : 'Android'} App
           </p>
           <a className="App-link" href={appScheme}>
             Click here to move back to the mobile app
